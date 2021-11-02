31910975f3cda78d34af118f077a322d3857676b
Stoil <stoil.pla4kov@gmail.com>
2021-11-02 15:56:14 +0200

    alo da

---
+++ BorrowLend/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BorrowLend
+{
+    public class Item
+    {
+        int Id;
+        string ItemName;
+        string Borrower;
+        string Lender;
+    }
+}
